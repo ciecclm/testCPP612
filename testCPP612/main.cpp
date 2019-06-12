c5020a66b59c0209c3e02898bc8f3afd253a339c
ciecclm <clm@imnu.edu.cn>
2019-06-12 21:00:46 +0800

    Initial Commit

---
+++ testCPP612/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  testCPP612
+//
+//  Created by 朝力萌 on 6/12/19.
+//  Copyright © 2019 朝力萌. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
